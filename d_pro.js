c95f286c5db357b3e609576cbff7f932fa90ade6
Peter Menocal <petermenocal@users.noreply.github.com>
2020-02-11 13:05:11 -0800

    fdsa
    dosa

---
+++ d_pro.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',[#22212C,#454158,#9580FF,#22212C,#454158,#FFFFFF,#8AFF80,#FF5555]);
+t.prefs_.set('foreground-color', "#fff");
+t.prefs_.set('background-color', "#22212C");
+t.prefs_.set('cursor-color', 'rgba(187,187,187,0.5)');
